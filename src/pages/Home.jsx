72b9ecc0bb8b1431e0fc426d898cf2154c9eca51
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-06-01 21:05:31 +0300

    add-pages-home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import { Typography, Box } from '@mui/material';
+import { titleStyle, centredItemsStyles, wrapperStyle } from 'shared/basicStyles';
+import Icon from 'img/follow.png';
+
+export const Home = () => {
+  return (
+    <Box>
+      <Box
+        sx={{
+          ...centredItemsStyles,
+          gap: '30px',
+          justifyContent: 'flex-start',
+          mb: '28px',
+          flexWrap: 'wrap',
+        }}
+      >
+        <img src={Icon} alt="" width={90} height={90} />
+        <Typography component="h1" sx={titleStyle}>
+          Tweets
+        </Typography>
+      </Box>
+
+      <Box sx={wrapperStyle}></Box>
+    </Box>
+  );
+};
