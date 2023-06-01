b727ed406c2612732a4e0bee859319cb0bff3a6a
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-06-01 23:02:36 +0300

    +

--- src/components/ToolsBar/ToolsBar.jsx
+++ src/components/ToolsBar/ToolsBar.jsx
@@ -1,5 +1,5 @@
 import { Box } from '@mui/material';
-import { wrapperStyle } from './toolsBarSytles';
+import { wrapperStyle } from './toolsBarStyles';
 
 export const ToolsBar = ({ children }) => {
   return <Box sx={wrapperStyle}>{children}</Box>;
