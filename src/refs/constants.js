6f5374ff64e2be9b6934b00c9717f3cf92e738b2
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-06-01 22:01:51 +0300

    +

---
+++ src/refs/constants.js
@@ -0,0 +1,18 @@
+export const statusFilters = ['Show all', 'Follow', 'Followings'];
+
+export const routesList = [
+  { name: 'Main', path: '/' },
+  { name: 'Tweets', path: '/tweets' },
+];
+
+export const lsKeys = Object.freeze({
+  users: 'users',
+  filter: 'filter',
+  followings: 'followings',
+});
+
+export const limit = 9;
+
+export const totalItems = 90;
+
+export const baseUrlApi = 'https://6478e79b362560649a2e9fa7.mockapi.io/api/';
